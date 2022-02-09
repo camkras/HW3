1348209c121ec5ed85fc7defce33e8c633fc77fe
noahpk2 <noah@pearsonkramer.com>
2022-02-09 12:10:31 -0700

    created files

---
+++ HW3Server.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.*;
+public class HW3Server {
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverTCPSocket = null;
+        boolean listening = true;
+        
+
+    }
+    
+
+
+}

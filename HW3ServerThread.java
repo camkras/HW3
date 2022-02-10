628d1bb05d70e187c462d0c68e8876683dde87b9
noahpk2 <noah@pearsonkramer.com>
2022-02-10 14:12:53 -0700

    update

--- HW3ServerThread.java
+++ HW3ServerThread.java
@@ -8,6 +8,7 @@
 //copied these from the example programs, now need to be changed to fit the assignment
 import java.net.*;
 import java.io.*;
+import java.util.Date;
 
 public class HW3ServerThread extends Thread {
     private Socket clientTCPSocket = null;
@@ -18,6 +19,7 @@ public class HW3ServerThread extends Thread {
     }
 
     public void run() {
+        Date date = new Date();
 
         try {
             PrintWriter cSocketOut = new PrintWriter(
@@ -41,25 +43,41 @@ public class HW3ServerThread extends Thread {
                 
                 if (line[0].equals("GET"))
                     {
-
+                        try{
+                            String path ="/HW03/Resources/"+ line[1];  // 'Resources' could be a separate folder or just store everything in HW03   
+                            BufferedReader br = new BufferedReader(new FileReader(path));
+                            //call another method? or everything inside try block/ if statement
+                            
+                            //Construct Response Message
+                            // Format: 
+                            /*          Response Header:
+                                HTTP/1.2 200 OK
+                                Date:    .... java date or calendar class
+                                Server:  .... how do we get the server name? should be able to change based on server
+                                \n
+                            */
+                            
+                            String response = "     Response Header \r\n" + line[2] +"\r\n"+"200 Okay \r\n"+"Date:"+date.toString()+"\r\n"+"Server:"; //needs to get server name
+                            
+        
+        
+        
+                            }
+                        catch (IOException e)
+                            {
+                                System.err.println(e);
+                                System.out.println("404 Not Found "); // output to client.
+                            }  
                     }
                 else
                     {
-                        System.out.println("400 Bad Request");
+                        System.out.println("400 Bad Request"); 
                         //needs to respond to client with this
                     }
 
                 
-                try{
-                    String path = line[1];     
-                    BufferedReader br = new BufferedReader(new FileReader(path));
-                    
-                    }
-                catch (IOException e)
-                    {
-                        System.err.println(e);
-                        System.out.println("404 Not Found "); // output to client.
-                    }
+                
+                
 
 
                 

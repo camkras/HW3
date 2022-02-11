e7584d27c0db01b16d599b7db6359139399690d6
noahpk2 <noah@pearsonkramer.com>
2022-02-10 23:09:54 -0700

    Todo

--- HW3Client.java
+++ HW3Client.java
@@ -77,6 +77,9 @@ public class HW3Client {
          
          
          /*
+
+
+         // TODO : READ .HTM FIlE, SAVE TO NEW FILE
                              
          if ((fromServer = socketIn.readLine()) != null)
          {

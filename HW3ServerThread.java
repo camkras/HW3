cc99b7cbad82b42fcda05aa89f94654ce2f79524
noahpk2 <noah@pearsonkramer.com>
2022-02-10 22:27:04 -0700

    updates

--- HW3ServerThread.java
+++ HW3ServerThread.java
@@ -5,7 +5,6 @@
    Weiying Zhu
 */
 
-//copied these from the example programs, now need to be changed to fit the assignment
 import java.net.*;
 import java.io.*;
 import java.util.Date;
@@ -25,29 +24,18 @@ public class HW3ServerThread extends Thread {
             PrintWriter cSocketOut = new PrintWriter(
                 clientTCPSocket.getOutputStream(), true);
 
-            BufferedReader cSocketIn = new BufferedReader(
-                new InputStreamReader(clientTCPSocket.getInputStream()));
-
-            
+            BufferedReader cSocketIn = new BufferedReader(new InputStreamReader(clientTCPSocket.getInputStream()));
             String HTTPRequest;
-            
             //read request from client             
             while ((HTTPRequest = cSocketIn.readLine()) != null) {
-                
-
                 //The client request can be combined into a single string, and then parsed.
-                
                 String[] requestLines = HTTPRequest.split("\r\n"); // maybe parse by comma... can we parse by \r\n ?
-                
                 String[] line = requestLines[0].split(" "); //parse by space
-                
                 if (line[0].equals("GET"))
                     {
                         try{
                             String path ="/HW03/Resources/"+ line[1];  // 'Resources' could be a separate folder or just store everything in HW03   
                             BufferedReader br = new BufferedReader(new FileReader(path));
-                            //call another method? or everything inside try block/ if statement
-                            
                             //Construct Response Message
                             // Format: 
                             /*          Response Header:
@@ -56,12 +44,22 @@ public class HW3ServerThread extends Thread {
                                 Server:  .... how do we get the server name? should be able to change based on server?
                                 *blank line*
                             */
-                            
-                            String response = "     Response Header \r\n" + line[2] +"\r\n"+"200 Okay \r\n"+"Date:"+date.toString()+"\r\n"+"Server:"; // still needs to get server name
-                            cSocketOut.println(response);
-                            
-        
-        
+                            InetAddress ip;
+                            String hostName = null;
+
+                                try {
+                                    ip = InetAddress.getLocalHost();
+                                    hostName = ip.getHostName();
+                                } catch (UnknownHostException e) {
+                                    e.printStackTrace();
+                                }
+                            String responseHeader = "     Response Header \r\n" + line[2] + "200 Okay \r\n"+"Date: "+date.toString()+"\r\n"+"Server: " + hostName +"\r\n"; // still needs to get server name
+                            cSocketOut.println(responseHeader);
+                            // Send .htm file
+                            while(br.readLine()!= null)
+                            {
+                                cSocketOut.write(br.readLine());
+                            }
                             }
                         catch (IOException e)
                             {
